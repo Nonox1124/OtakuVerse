4bf79d225b7ce1d6130ef947e8ab82b1b55a5e20
Nonox <noa.calvet@epitech.eu>
2024-12-07 11:08:32 +0100

    [ADD] Basic design signUp

---
+++ OtakuVerse/Pages/SignUpPage.swift
@@ -0,0 +1,63 @@
+//
+//  SignUpPage.swift
+//  OtakuVerse
+//
+//  Created by Noa Calvet on 06/12/2024.
+//
+
+import SwiftUI
+
+struct SignUpPage: View {
+    
+    @State private var username: String = ""
+    @State private var password: String = ""
+    @State private var confirmPassword: String = ""
+    @State private var email: String = ""
+    @State private var confirmEmail: String = ""
+    @State private var isSignUp: Bool = false
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        NavigationStack {
+            ZStack {
+                Color.darkBlue.ignoresSafeArea()
+                VStack {
+                    Image(systemName: "person.crop.circle.badge.plus")
+                        .font(.system(size: 100))
+                        .foregroundStyle(Color.ligthPurple)
+                    Spacer()
+                    TextField("Username", text: $username)
+                        .textFieldStyle(.roundedBorder)
+                        .textContentType(.username)
+                        .font(.system(size: 30))
+                    Spacer()
+                    Group {
+                        TextField("Email", text: $email)
+                        TextField("Confirm Email", text: $email)
+                    }
+                    .textContentType(.emailAddress)
+                    .keyboardType(.emailAddress)
+                    .textFieldStyle(.roundedBorder)
+                    .font(.system(size: 30))
+                    Spacer()
+                    Group {
+                        TextField("Password", text: $password)
+                        TextField("Confirm Password", text: $confirmPassword)
+                    }
+                    .textContentType(.password)
+                    .textFieldStyle(.roundedBorder)
+                    .font(.system(size: 30))
+                    Spacer()
+                    NavigationLink(destination: HomePage(currentPage: "HomePage")) {
+                        Text("Sign Up")
+                    }
+                }
+                .padding()
+            }
+        }
+    }
+}
+
+#Preview {
+    SignUpPage()
+}
