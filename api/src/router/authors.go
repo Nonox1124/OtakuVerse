08fd52cd7b8867aacda4de6da9c56e8609b21257
gonzter <guillaume.rocchia@epitech.eu>
2025-01-07 20:25:38 +0100

    Setup router functions

---
+++ api/src/router/authors.go
@@ -0,0 +1,21 @@
+package router
+
+import (
+    "github.com/gin-gonic/gin"
+)
+
+func HelperGetCredentialsUser(c *gin.Context) {
+    // non implemented func
+}
+
+func HelperPostCredentialsUser(c *gin.Context) {
+    // non implemented func
+}
+
+func HelperDeleteCredentialsUser(c *gin.Context) {
+    // non implemented func
+}
+
+func HelperPatchCredentialsUser(c *gin.Context) {
+    // non implemented func
+}
---
+++ api/src/router/credentials.go
@@ -0,0 +1,18 @@
+package router
+
+import (
+    "github.com/gin-gonic/gin"
+)
+
+
+func HelperGetUser(c *gin.Context, username string) {
+    // non implemented func
+}
+
+func HelperDeleteUser(c *gin.Context, username string, bookID int) {
+    // non implemented func
+}
+
+func HelperPatchUser(c *gin.Context, username string, bookID int) {
+    // non implemented func
+}
